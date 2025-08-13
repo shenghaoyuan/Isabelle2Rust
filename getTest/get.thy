5d9a6c460c482bb820dd7027c402c93edf8d868e
fisherfg19 <FisherFG@outlook.com>
2025-08-13 18:12:24 +0800

    feat(print_constructor): box recursively defined enum element

--- getTest/get.thy
+++ getTest/get.thy
@@ -4,12 +4,13 @@ theory get
 begin
 
 
-datatype  option = None | Some int
+datatype  option = None | Some int | Rec option
 
 
 fun get :: "option \<Rightarrow> int" where
 " get (Some x) = x"| 
-" get None = 0"
+" get None = 0" |
+" get (Rec op) = get op" 
 
 export_code get in Rust
 export_code get in OCaml
