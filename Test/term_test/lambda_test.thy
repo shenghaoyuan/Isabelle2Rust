1d3aa64e244b442abfe5718f00a717aa7e1aab17
fisherfg19 <FisherFG@outlook.com>
2025-11-18 17:28:44 +0800

    feat(print_term, print_def): translate lambda to closure and function-type to closure trait impl

--- Test/term_test/lambda_test.thy
+++ Test/term_test/lambda_test.thy
@@ -21,7 +21,26 @@ definition inc :: "int \<Rightarrow> int" where
 definition add_n :: "int \<Rightarrow> (int \<Rightarrow> int)" where
   "add_n n \<equiv> (\<lambda>x. x + n)"
 
+ definition add_n_2 ::  "int \<Rightarrow> (int \<Rightarrow> (int \<Rightarrow> int))" where
+  "add_n_2 n \<equiv> (\<lambda>x. (\<lambda>y. x + y + n))"
 
+ definition add_n_3 ::  "int \<Rightarrow> (int \<Rightarrow> int \<Rightarrow> int)" where
+  "add_n_3 n \<equiv> (\<lambda>x y. x + y + n)"
+
+definition test_add_n_2 :: "int \<Rightarrow> (int \<Rightarrow> int)" where
+  "test_add_n_2 x \<equiv> (\<lambda>y. ((add_n_2 1) x) y)"
+
+definition int_pair :: "int \<Rightarrow> (int \<times> int)" where
+  "int_pair x = (x, x)"
+
+definition app :: "(int \<Rightarrow> int) \<Rightarrow> int \<Rightarrow> int" where
+  "app f x = f x"
+
+definition app2 :: "int \<Rightarrow> (int \<Rightarrow> int) \<Rightarrow> int" where
+  "app2 x f = f x"
+
+definition add3 :: "int \<Rightarrow> int \<Rightarrow> int \<Rightarrow> int" where
+  "add3 x y z = x + y + z"
 
 export_code id in OCaml   
 export_code id in Rust
@@ -30,4 +49,10 @@ export_code inc in Rust
 
 export_code add_n in OCaml   
 export_code add_n in Rust
+
+export_code add_n_2 add_n_3 test_add_n_2 in OCaml   
+export_code add_n_2 add_n_3 test_add_n_2 in Rust
+
+export_code add_n int_pair app app2 add3 in OCaml   
+export_code add_n int_pair app app2 add3 in Rust
 end
