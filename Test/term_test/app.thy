bc4b6e72e95c4dda4be8ef4b85a15853fb48917f
fisherfg19 <FisherFG@outlook.com>
2025-11-25 00:58:04 +0800

    feat(print_term): detect and add Box::new correctly

--- Test/term_test/app.thy
+++ Test/term_test/app.thy
@@ -18,13 +18,10 @@ definition test2 :: "int" where
   "test2 = add 3 4"
 
 
-definition test3 :: "int \<Rightarrow> int" where
-  "test3 = add 3 4 "
-
 fun add3 :: "int \<Rightarrow> int \<Rightarrow> int \<Rightarrow> int" where
   "add3 x y z = (add x y) + z"
 
-definition test3' :: "int" where
-  "test3' = add3 3 4 5"
+definition test3 :: "int" where
+  "test3 = add3 3 4 5"
 
 end
