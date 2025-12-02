3a41230970f7994165132d16da1e4c55d6c9d656
lujiayi <lucia20021008@gmail.com>
2025-12-02 17:12:33 +0800

    test: add targeted tests

---
+++ tests_targeted/Class_Semigroup_Nat2_Test.thy
@@ -0,0 +1,41 @@
+theory Class_Semigroup_Nat2_Test
+  imports Main "Rust.Rust_Setup"
+begin
+
+class semigroup =
+  fixes plus :: "'a \<Rightarrow> 'a \<Rightarrow> 'a" (infixl "+s" 65)
+
+
+class monoid = semigroup +
+  fixes zero :: "'a"
+
+
+datatype Nat = Zero | Suc Nat
+
+instantiation Nat :: semigroup
+begin
+
+fun plus_Nat :: "Nat \<Rightarrow> Nat \<Rightarrow> Nat" where
+  "a +s Zero = a"
+| "Zero +s a = a"
+| "Suc a +s b = Suc (a +s b)"
+instance ..
+
+end
+
+instantiation Nat :: monoid
+begin
+
+definition zero_Nat :: "Nat" where
+  "zero = Zero"
+
+instance ..
+end
+
+
+fun sum :: "('a :: monoid) list \<Rightarrow> 'a" where
+  "sum xs = fold plus xs zero"
+
+export_code sum in Rust
+
+end
